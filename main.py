0b3edd84633cfe3eda9edd219447948177cce051
ArtemVikharev <webavikharev@gmail.com>
2024-07-15 20:16:56 +0300

    complete feature/mainLayout

--- main.py
+++ main.py
@@ -1,36 +1,17 @@
 import sys
 import random
 from PySide6 import QtCore, QtWidgets
+from PySide6.QtWidgets import QGroupBox
 
 
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
-        super().__init__()
+        super().__init__()        
 
-        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
-
-        
-        
-
-        self.layout = QtWidgets.QVBoxLayout(self)
-        
-
-    def init_button(self):
-        self.button = QtWidgets.QPushButton("Click me!")
-        self.layout.addWidget(self.button)
-        self.button.clicked.connect(self.magic)
-
-    def init_textarea(self):
-        self.layout.addWidget(self.text)
-        self.text = QtWidgets.QLabel("Hello World",
-                                     alignment=QtCore.Qt.AlignCenter)
-
-    def magic(self):
-        self.text.setText(random.choice(self.hello))
-        
-        
-
-
+        self.main_layout = QtWidgets.QVBoxLayout(self)
+        self.title =  QtWidgets.QLabel("Cписок альбомов",
+                                     alignment=QtCore.Qt.AlignHCenter)
+        self.main_layout.addWidget(self.title)
 
 app = QtWidgets.QApplication([])
 
